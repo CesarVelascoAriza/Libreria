9ca450a1de0d2498b362b256a4991ce8342c5764
cesar velasco <caacesar.ariza@gmail.com>
2023-08-30 14:37:03 -0500

    cambio de release

---
+++ DominioNegocio/service-books/bin/target/classes/import.sql
@@ -0,0 +1,4 @@
+insert into categorias (description) values('romance');
+insert into categorias (description) values('drama');
+insert into categorias (description) values('comedia');
+insert into libros (title,sub_title,date_publication,price,categoria_id,create_at) values ('listo','sub list', NOW(),9000,1,NOW());
--- DominioNegocio/service-books/src/main/java/com/cava/examples/services/book/modelError/ModelError.java
+++ DominioNegocio/service-books/src/main/java/com/cava/examples/services/book/modelError/ModelError.java
@@ -12,4 +12,36 @@ public class ModelError {
 
     public ModelError() {
     }
+
+	public ModelError(int code, String message, String messageError) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.messageError = messageError;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessageError() {
+		return messageError;
+	}
+
+	public void setMessageError(String messageError) {
+		this.messageError = messageError;
+	}
+    
 }
