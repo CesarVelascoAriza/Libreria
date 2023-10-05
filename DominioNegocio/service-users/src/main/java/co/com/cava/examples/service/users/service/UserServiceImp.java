65ef23d338092460a96b7f25c49a9d078a3d92c6
cesar <caacesar.ariza@gmial.com>
2023-10-04 21:19:09 -0500

    Docker
    Docker compse y docer

--- DominioNegocio/service-users/src/main/java/co/com/cava/examples/service/users/service/UserServiceImp.java
+++ DominioNegocio/service-users/src/main/java/co/com/cava/examples/service/users/service/UserServiceImp.java
@@ -11,15 +11,7 @@ import co.com.cava.examples.service.users.repository.UserRespository;
 @Service
 public class UserServiceImp extends CommonServiceImp<Usuario,UserRespository> implements UserService {
 
-    public Usuario updateUser(Usuario user) {
-        return repository.findById(user.getId()).map(m->{
-          m.setUserName(user.getUserName());
-          m.setPassword(user.getPassword());
-          m.setRoles(user.getRoles());
-          
-          return  m;
-        }).orElseThrow();
-    }
+
     @Override
     public Optional<Usuario> findByName(String userName) {
         return repository.findByUserName(userName);
