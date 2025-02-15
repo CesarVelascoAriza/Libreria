45d7b0a1840933dcea6180c2dd922b7326bcf8d4
CesarVelascoAriza <caacesar.ariza@gmail.com>
2025-02-15 02:57:48 +0000

    configuaciones

--- Infraestructura/getway-server/src/main/java/com/example/getwayserver/securityconfig/SecurityConfigResource.java
+++ Infraestructura/getway-server/src/main/java/com/example/getwayserver/securityconfig/SecurityConfigResource.java
@@ -1,29 +1,56 @@
 package com.example.getwayserver.securityconfig;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfigResource {
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception{
         http
-                .authorizeExchange(authorize -> authorize
-                        .pathMatchers("/api/security/**", "/actuator/**").permitAll()
-                        .pathMatchers("/api/users").hasRole("ADMIN")
-                        .anyExchange().authenticated()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/security/**", "/actuator/**").permitAll()
+                        .requestMatchers( "/api/users/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
                 )
+                .cors( crf -> crf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2Login(Customizer.withDefaults())
-                .oauth2ResourceServer(resource -> {
-                    resource.jwt(Customizer.withDefaults());
-                })
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                    jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, AbstractAuthenticationToken>() {
+
+                        @Override
+                        public AbstractAuthenticationToken convert(Jwt source) {
+                            Collection<String> roles = source.getClaimAsStringList("roles");
+                            Collection<GrantedAuthority> authorities = roles.stream()
+                            .map(SimpleGrantedAuthority::new)
+                                    .collect(Collectors.toList());
+
+                            return new JwtAuthenticationToken(source, authorities);
+                        }
+                    })))
         ;
-        http.csrf(csrf -> csrf.disable());
+        
+
         return http.build();
     }
 }
